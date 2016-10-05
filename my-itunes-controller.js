9dbed2a77d7811d063c14b392745bb8df235e68b
Chad Rohr <chadrohr@yahoo.com>
2016-10-05 17:17:27 -0600

    EOC Wed

--- my-itunes-controller.js
+++ my-itunes-controller.js
@@ -1,16 +1,15 @@
 //function MytunesController() {
 
-var myTunes = new MytunesService()
+var myTunes = new MyTunesService()
 
 //Do Not Modify the getMusic function
 function getMusic() {
     var artist = document.getElementById('artist').value;
     itunes.getMusicByArtist(artist).then(drawSongs);
 }
-// console.log(getTracks.artist.songList.length)
 
 function drawSongs(songList) {
-    //console.log(songList);
+    myTunes.putTracks(songList)
     var template = ""
     var songElement = document.getElementById('songs')
     for (var i = 0; i < songList.length; i++) {
@@ -33,19 +32,54 @@ function drawSongs(songList) {
                             <audio controls style="width=250px" ; " src="${song.preview} ">
                                 <a href="${song.preview} " class="card-link ">Card link</a>
                             </audio>
-                            <button type="button" class="btn btn-success" id="${song.id}" onclick="myTunes.addTrack()">Add to Playlist</button>
+                            <button class="btn btn-success btn-sm" type="button" id="${song.id}" onclick="myTunes.addTrack(${song.id})">Add to Playlist</button>
                         </div>
                     </div>
                 </div>`
     }
     songElement.innerHTML = template
 }
+function drawMySongs(myList){
+    var template = ""
+    var songElement = document.getElementById('my-songs')
+    for (var i = 0; i < songList.length; i++) {
+        var song = songList[i]
+        template +=
+            ` <div class="card">
+                    <div class="card-block">
+                        <div class="col-sm-1">
+                            <img class="card-img-left" src="${song.albumArt}" alt="Card image cap">
+                        </div>
+                        <div class="name-artist col-sm-4">
+                            <h5 class=" card-title ">${song.title}</h5>
+                            <p class="card-text ">${song.artist}</p>
+                        </div>
+                        <div class="col-sm-3 ">
+                            <p class="card-text ">${song.collection}</p>
+                            <p class="card-text "><a href="_blank " class="card-link ">Buy Now</a> $${song.price}</p>
+                        </div>
+                        <div class="sound-box col-sm-4 ">
+                            <audio controls style="width=250px" ; " src="${song.preview} ">
+                                <a href="${song.preview} " class="card-link ">Card link</a>
+                            </audio>
+                            <button class="btn btn-danger btn-sm" type="button" id="${song.id}" onclick="myTunes.removeTrack(${song.id})">Remove from Playlist</button>
+                            <button class="btn btn-info btn-sm" type="submit">Promote in List</button>
+                            <button class="btn btn-info btn-sm" type="submit">Demote in List</button>
+                        </div>
+                    </div>
+                </div>`
+}
+songElement.innerHTML = template
+}
+
+
 window.addEventListener("play", function (evt) {
     if (window.$_currentlyPlaying) {
         window.$_currentlyPlaying.pause();
     }
     window.$_currentlyPlaying = evt.target;
 }, true);
-    //drawSongs(songs)
+    drawSongs(myTunes.masterPlayList)
+    drawMySongs(myTunes.myPlayList)
 //}
 
--- my-itunes-service.js
+++ my-itunes-service.js
@@ -1,30 +1,81 @@
 function MyTunesService() {
   var myTracks = loadTracks();
-  var myPlayListTracks = [];
-
-  // pulls from itunes add builds itunesTracks
+  var myPlayList = []
+  var masterPlayList = []
+  // pulls from itunes add builds PlayList sending to myTacks
+  this.putTracks = function (arr) {
+    masterPlayList = arr
+    localStorage.setItem('masterPlayList', JSON.stringify(arr))
+  }
   this.getTracks = function () {
+    return loadTracks()
 
-    
   }
   // Adds to my personal Playlist --myTracks
-  this.addTrack = function () {
-
+  this.addTrack = function (id) {
+    // if(myPlayList.length == 0){
+    //   myPlayList.push(song);
+    // }
+    for (var i = 0; i < masterPlayList.length; i++) {
+      var song = masterPlayList[i];
+      if (song.id == id) {
+        myPlayList.push(song);/// Do I build full song here
+        masterPlayList.splice(i, 1);
+      }
+    }
+    saveTracks(myPlayList)
+    return
   }
 
   // Removes track from my personal Playlist --myTracks
   this.removeTrack = function () {
-
+    for (var i = 0; i < myPlayList.length; i++) {
+      var song = myPlayList[i];
+      if (song.id == id) {
+        myPlayList.splice(i, 1);
+        masterPlayList.push(song);
+      }
+    }
+    saveTracks(myPlayList)
+    return
   }
 
-  // Moves track up in my personal Playlist --myTracks
+  //Moves track up in my personal Playlist --myTracks
   this.promote = function () {
-
+    for (var i = 0; i < myPlayList.length; i++) {
+      if (myPlayList[i].id == id) {
+        break;
+      }
+    }
+    if (i > 0) {
+      myPlayList.splice(i - 1, 0, myPlayList.splice(i, 1)[0]);
+    }
   }
 
   //moves track down in my personal Playlist --myTracks
-  this.demote.Track = function () {
-
+  this.demoteTrack = function () {
+    for (var i = 0; i < myPlayList.length; i++) {
+      if (myPlayList[i].id == id) {
+        break;
+      }
+    }
+    myPlayList.splice(i + 1, 0, myPlayList.splice(i, 1)[0]);
   }
-
+  this.clearData = function () {
+    localStorage.removeItem('myPlayList')
+  }
+  function saveTracks(myTracks) {
+    localStorage.setItem('myPlayList', JSON.stringify(myTracks))
+  }
+  function loadTracks() {
+    myPlayList = localStorage.getItem('myPlayList')
+    if (myPlayList) {
+      myPlayList = JSON.parse(myPlayList)
+    } else {
+      // Which List should I pull from
+      myPlayList = []//songs from somewhere
+      // array of songs 
+    }
+    return myPlayList
+  }
 }
